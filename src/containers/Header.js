a26d1171255fd3275d37ebba0c03e5c2f2d62306
thoriqnoer <thoriq.noer@gmail.com>
2022-01-26 08:44:28 +0700

    create header component

---
+++ src/containers/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="ui stackable menu">
+      <div className="ui container center">
+        <div className="item">
+          <h2>Fakeshop</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
