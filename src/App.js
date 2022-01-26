3a4d6b4ac37735942f92e5d23f71857cf6ca9917
thoriqnoer <thoriq.noer@gmail.com>
2022-01-26 08:43:45 +0700

    Create routing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductPage from './containers/ProductPage';
+import ProductDetails from './containers/ProductDetails';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductPage />} />
+          <Route path="product">
+            <Route path=":productId" element={<ProductDetails />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
